57494e9f8f13d768fb865b3194dbf0b3524994ed
Gilles BOYER <boyer.gilles@live.fr>
2022-04-11 08:09:59 +0400

    ajout controle seed

--- database/factories/folder.factory.js
+++ database/factories/folder.factory.js
@@ -1,16 +1,27 @@
 const { faker } = require('@faker-js/faker');
 const Folder = require("../../models/Folder.Model");
 
+function VerifySeed(model) {
+    var res = model.find()
+
+    if (res != []) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 module.exports.Factory = function(nbr) {
     var tab = []
 
-    for (let x = 0; x < nbr; x++) {
-        var folder = new Folder({
-            label: faker.company.companyName()
-        });
+    if (VerifySeed(Folder)) {
+        for (let x = 0; x < nbr; x++) {
+            var folder = new Folder({
+                label: faker.company.companyName()
+            });
 
-        tab.push(folder);
+            tab.push(folder);
+        }
     }
-
     return tab;
 }
--- database/seeders/seed.js
+++ database/seeders/seed.js
@@ -1,13 +1,19 @@
 const folder = require("../factories/folder.factory");
 
 function seed(nbr, factory) {
+
     var tab = factory.Factory(nbr);
-    tab.forEach(element => {
-        element.save((err, element) => {
-            if (err) return console.log(err);
-            return console.log(element);
-        })
-    });
+
+    if (tab.length > 0) {
+        tab.forEach(element => {
+            element.save((err, element) => {
+                if (err) return console.log(err);
+                return console.log(element);
+            })
+        });
+    } else {
+        return console.log('this model is already seed');
+    }
 }
 
 module.exports.seeders = (req, res) => {
