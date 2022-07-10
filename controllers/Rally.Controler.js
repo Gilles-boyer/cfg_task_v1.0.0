0acd4f10906230b4a1dfa47070979a08dfa60f4c
Gilles BOYER <boyer.gilles@live.fr>
2022-07-10 10:43:26 +0400

    ajout admin planning

--- controllers/Rally.Controler.js
+++ controllers/Rally.Controler.js
@@ -30,6 +30,20 @@ module.exports.store = (req, res) => {
     });
 };
 
+module.exports.update = (req, res) => {
+    if(!req.body.id){
+        return res.status(500).json({error:"erreur Id"});
+    }
+    Resa.findByIdAndUpdate(req.body.id, {
+        dateRoulage : req.body.date,
+        matinOuSoir : req.body.moment,
+        nbrPilote   : req.body.nbrPilote
+    }, function (err, resa) {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json(resa);
+    })
+};
+
 module.exports.addPilote = (req, res) => {
     var pilotes = [];
     req.query.rallyId = req.query.rallyId.split(' ');
@@ -62,12 +76,12 @@ module.exports.addPilote = (req, res) => {
 var selectFormuleById = (id) => {
     if(id == 1) {
         return {
-            name: "1 sessions 10min => 25€",
+            name: "1 session 10min => 25€",
             nbr:1
         };
     } else if (id == 2) {
         return {
-            name: "3 sesions 10min => 60€",
+            name: "3 sessions 10min => 60€",
             nbr:3
         };
     } else if (id == 3) {
@@ -97,5 +111,66 @@ module.exports.updateSession = async (req, res) => {
     await pilote.save((err, updatePilote) => {
         if (err) return res.status(500).json(err);
         return res.status(200).json(updatePilote);
+    });
+}
+
+module.exports.deleteSession = (req, res) => {
+    if(!req.params.id){
+        return res.status(500).json({error:"erreur Id"});
+    }
+    Resa.findByIdAndDelete(req.params.id, (err)=> {
+        if(err) return res.status(500).json(err);
+        res.status(200).json({
+            response : "1 document deleted"
+        })
+    });
+}
+
+module.exports.addPiloteAdmin = (req, res) => {
+    var pilote = new Pilote(
+        {
+            name        : req.body.name,
+            voiture     : req.body.voiture,
+            formule     : req.body.formule,
+            phone       : req.body.phone,
+            nbrSession  : req.body.nbrSession,
+            paiement    : req.body.paiement,
+            rally       : req.body.rally
+        }
+    );
+
+    pilote.save((err, newPilote) => {
+        if (err) return res.status(500).json(err);
+        return res.status(201).json(newPilote);
+    });
+}
+
+module.exports.updatePilote = (req, res) => {
+    if(!req.body.id){
+        return res.status(500).json({error:"erreur Id"});
+    }
+    Pilote.findByIdAndUpdate(req.body.id, {
+        name        : req.body.name,
+        voiture     : req.body.voiture,
+        formule     : req.body.formule,
+        phone       : req.body.phone,
+        nbrSession  : req.body.nbrSession,
+        paiement    : req.body.paiement,
+        rally       : req.body.rally
+    }, function (err, pilote) {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json(pilote);
+    })
+}
+
+module.exports.deletePilote = (req, res) => {
+    if(!req.params.id){
+        return res.status(500).json({error:"erreur Id"});
+    }
+    Pilote.findByIdAndDelete(req.params.id, (err)=> {
+        if(err) return res.status(500).json(err);
+        res.status(200).json({
+            response : "1 document deleted"
+        })
     });
 }
--- routes/resa-rally.js
+++ routes/resa-rally.js
@@ -4,7 +4,12 @@ var ResaRally = require("../controllers/Rally.Controler");
 
 router.get("/", ResaRally.index);
 router.post("/store/resa", ResaRally.store);
+router.post("/update/resa", ResaRally.update);
 router.get("/update/pilote", ResaRally.addPilote);
 router.get("/update/session/pilote/:id", ResaRally.updateSession);
+router.get("/delete/resa/:id", ResaRally.deleteSession);
+router.post("/store/pilote", ResaRally.addPiloteAdmin);
+router.post("/update/pilote/admin", ResaRally.updatePilote);
+router.get("/delete/pilote/:id", ResaRally.deletePilote);
 
 module.exports = router;
